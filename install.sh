cd430ca602b83fac347ef40d10708ab0c7577480
Nathan Burgess <nathanburgess@users.noreply.github.com>
2017-07-13 11:20:15 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -68,9 +68,9 @@ mkdir /home/${U}/.config
 chown -R $U:$(id -gn $U) /home/${U}/.config
 
 echo -e "\n\r\e[32mInstalling some Node.js packages\e[0m"
-su vagrant << 'EOF'
+#su vagrant << 'EOF'
 /usr/bin/npm i -g yarn nodemon laravel-echo-server 
-EOF
+#EOF
 
 mysql -uroot -psecret -e "CREATE DATABASE reactivegaming;"
 
